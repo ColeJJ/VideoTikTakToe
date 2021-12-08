ac3211e4b412bb5d10dd42d51b30dda888c80cd5
ColeJJ <torben.unland@gmail.com>
2021-12-08 16:23:00 +0100

    recreated gamecenter ejb

---
+++ VideoTikTakToe/VideoTikTakToe-ejb_gamecenter/src/main/java/com/videotiktaktoe/app/Gamecenter/entity/SpielsessionTO.java
@@ -0,0 +1,67 @@
+package com.videotiktaktoe.app.Gamecenter.entity;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import de.VideoTikTakToe.Gamecenter.entity.impl.Spielsession;
+import de.bank.kunde.entity.Integer;
+import de.bank.kunde.entity.String;
+import de.bank.kunde.entity.impl.Kunde;
+import de.bank.kunde.entity.impl.Privatkunde;
+
+public class SpielsessionTO implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private int sessionID;
+	private String anzahlRunde;
+	private int lobbyID;
+	
+	public SpielsessionTO() {
+		super();
+	}
+	
+	public SpielsessionTO(String sessionID, int anzahlRunde, int lobbyID) {
+		super();
+		this.sessionID = sessionID;
+		this.anzahlRunde = anzahlRunde;
+		this.lobbyID = lobbyID;
+	}
+	
+	public Spielsession toSpielsession() {
+		Spielsession spielsession = new Spielsession(
+//				this.getSessionID(),
+				this.getAnzahlRunde());
+				this.getLobbyID();
+				
+		return spielsession;
+	}
+	
+	public int getSessionID() {
+		return sessionID;
+	}
+
+	public void setSessionID(int sessionID) {
+		this.sessionID = sessionID;
+	}
+
+	public String getAnzahlRunde() {
+		return anzahlRunde;
+	}
+	
+	public void setAnzahlRunde(String anzahlRunde) {
+		this.anzahlRunde = anzahlRunde;
+	}
+	
+	public int getLobbyID() {
+		return lobbyID;
+	}
+
+	public void setLobbyID(int lobbyID) {
+		this.lobbyID = lobbyID;
+	}
+}

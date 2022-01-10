cfba441ea10c4c416576582d41657165e8c1fc7d
ColeJJ <torben.unland@gmail.com>
2022-01-10 14:11:42 +0100

    jitsi ist mit drin

---
+++ VideoTikTakToe/VideoTikTakToe-web/src/main/webapp/resources/js/jitsi.js
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+ 
+window.onload = () => {
+	const api = new JitsiMeetExternalAPI("8x8.vc", {
+	  roomName: "vpaas-magic-cookie-65e9dc1e683c479ab44c705afe448a8c/SampleAppImpossibleBellsBlockOverly",
+	  parentNode: document.querySelector('#game:jaas-container')
+	});
+}
